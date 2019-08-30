3de3680bc835cc00ffc68c38c22933802e0d86bf
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2019-08-30 14:14:48 +0900

    update StateBridge to adjust new r_state

--- src/uoa_poc2_bridge/state_bridge.py
+++ src/uoa_poc2_bridge/state_bridge.py
@@ -52,9 +52,28 @@ class StateBridge(MQTTBase):
                         'yaw': state.pose.angle.yaw,
                     },
                 },
+                'destination': {
+                    'point': {
+                        'x': state.destination.point.x,
+                        'y': state.destination.point.y,
+                        'z': state.destination.point.z,
+                    },
+                    'angle_optional': {
+                        'valid': state.destination.angle_optional.valid,
+                        'angle': {
+                            'roll': state.destination.angle_optional.angle.roll,
+                            'pitch': state.destination.angle_optional.angle.pitch,
+                            'yaw': state.destination.angle_optional.angle.yaw,
+                        }
+                    }
+                },
                 'covariance': [c for c in state.covariance],
                 'battery': {
                     'voltage': state.battery.voltage,
+                    'current_optional': {
+                        'valid': state.battery.current_optional.valid,
+                        'current': state.battery.current_optional.current,
+                    }
                 }
             }
 
